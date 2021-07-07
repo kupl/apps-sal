f48a1d4a5f8115f83e9d04c8ac06a96f9f52142e
Henry Lee <gbvrcx@gmail.com>
2021-07-07 13:10:17 +0000

    feat: make apps-sal installable

--- apps_sal/__init__.py
+++ apps_sal/__init__.py
@@ -3,3 +3,6 @@ from apps_sal.dataset import load_dataset
 
 logging.basicConfig(format='%(asctime)s app_sal [%(levelname)s] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
+
+
+__version__ = '0.0.1'
