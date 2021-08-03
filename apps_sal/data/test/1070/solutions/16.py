5794e56e8a1f75f571a42538a2a95cdf2d49be94
Henry Lee <gbvrcx@gmail.com>
2021-08-03 04:29:33 +0000

    refine: format solutions
    Format following solutions of following problems:
     * apps_sal/data/test/3538
     * apps_sal/data/test/2624
     * apps_sal/data/test/1070
     * apps_sal/data/test/2901
     * apps_sal/data/test/1100
     * apps_sal/data/test/3759
     * apps_sal/data/test/3249
     * apps_sal/data/test/1766
     * apps_sal/data/test/0664
     * apps_sal/data/test/4767

--- apps_sal/data/test/1070/solutions/16.py
+++ apps_sal/data/test/1070/solutions/16.py
@@ -1,6 +1,7 @@
 dom_color = [int(x) for x in input().split(' ')]
 dom = [int(x) for x in input().split(' ')]
-max1 = 1; max2 = 1
+max1 = 1
+max2 = 1
 for i in range(dom_color[0] - 1):
     if dom[i] != dom[i + 1]:
         max2 += 1
--- apps_sal/data/test/1766/solutions/5.py
+++ apps_sal/data/test/1766/solutions/5.py
@@ -1,6 +1,7 @@
 n = int(input())
 a = list(map(int, input().split()))
-s = 0; d = 0;
+s = 0
+d = 0
 for i in range(1, n + 1):
     maxi = max(a[0], a[-1])
     ind = a.index(maxi)
