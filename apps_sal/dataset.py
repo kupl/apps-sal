76b41380272d50cb85b653dc88c47132fa976db3
Henry Lee <gbvrcx@gmail.com>
2022-02-17 05:49:55 +0000

    fix: format id only when id is integer

--- apps_sal/dataset.py
+++ apps_sal/dataset.py
@@ -43,7 +43,8 @@ class Dataset:
         return '\n'.join((elem.to_json(*args, **kwargs) for elem in self.data))
 
     def query(self, id: Union[int, str]) -> Union[DataElement, None]:
-        id = f'{id:04d}'
+        if not isinstance(id, str):
+            id = f'{id:04d}'
         for elem in self.data:
             if str(elem.path).endswith(id):
                 return elem
