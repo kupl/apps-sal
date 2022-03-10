cca266a729ebc7dfba78fced613ff656367fb866
Henry Lee <gbvrcx@gmail.com>
2022-03-10 08:47:13 +0000

    feat: evaluation mode

--- apps_sal/bin.py
+++ apps_sal/bin.py
@@ -32,8 +32,9 @@ def main(argv=None):
         argv = sys.argv[1:]
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--set', default='test', choices=['train', 'test'], help='set to use')
-    parser.add_argument('-t', '--target', default=None, type=str, metavar='<*.jsonl|*.json>', help='generated programs')
+    parser.add_argument('-m', '--mode', default='eval', choices=['save', 'eval'], help='mode (default=eval)')
+    parser.add_argument('-s', '--set', default='test', choices=['train', 'test'], help='set to use (default=test)')
+    parser.add_argument('-t', '--target', default='apps.jsonl', type=str, metavar='<*.jsonl|*.json>', help='file that has program data (apps.jsonl)')
     args = parser.parse_args(argv)
 
     dataset_loader = {
@@ -41,3 +42,34 @@ def main(argv=None):
         'test': load_test_dataset
     }[args.set]
     dataset = dataset_loader()
+
+    if args.mode == 'save':
+        pass
+
+    # if args.mode == 'eval'
+    else:
+        target = Path(args.target)
+        target_loader = {
+            '.jsonl': load_jsonl,
+            '.json': load_json
+        }[target.suffix]
+        target = target_loader(target)
+
+        result = {}
+        for key, programs in target.items():
+            print(f'Problem: {key}')
+            problem = dataset.query(key)
+            for i, program in enumerate(programs):
+                print(f'\rStatus: evaluating candidate {i + 1}', end='', flush=True)
+                score = problem.score(program)
+                if score == 1.0:
+                    result[key] = 'solved'
+                    break
+            else:
+                result[key] = 'failed'
+            print(f'\rStatus: {result[key]}                        ')
+            print()
+
+        total = len(target)
+        solved = sum((1 for r in result if r == 'solved'))
+        print(f'Solve rate: {solved} / {total} ({solved / total * 100:.2f}%)')
